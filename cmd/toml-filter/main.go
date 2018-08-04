df794e61508d06001e7a642d518f083c21e4cf60
Sanemat <o.gata.ken@gmail.com>
2018-08-04 17:29:36 +0700

    chore(app): add example

--- cmd/toml-filter/main.go
+++ cmd/toml-filter/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
 
 func main() {
+	var tmp interface{}
+	if _, err := toml.DecodeReader(os.Stdin, &tmp); err != nil {
+		log.Fatalf("Error decoding TOML: %s", err)
+	}
 	fmt.Println("hello world")
 }
